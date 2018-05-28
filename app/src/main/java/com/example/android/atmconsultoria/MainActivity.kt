24738f12f32251419b00a57cfe61bfff938e45eb
Ytallo de Lima Gadelha <ytallolg@gmail.com>
2018-05-27 23:10:17 -0300

    Adição do ícone do aplicativo e da funcionalidade de mensagem TOAST(botões)

--- app/src/main/java/com/example/android/atmconsultoria/MainActivity.kt
+++ app/src/main/java/com/example/android/atmconsultoria/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.android.atmconsultoria
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -13,11 +14,15 @@ class MainActivity : AppCompatActivity() {
 
         button_contato.setOnClickListener {
 
+            Toast.makeText(this, "Contato foi clicado!", Toast.LENGTH_SHORT).show()
+
             val intent = Intent(this, DetalheContatoActivity::class.java)
             startActivity(intent)
         }
 
         button_empresa.setOnClickListener {
+
+            Toast.makeText(this, "Empresa foi clicada!", Toast.LENGTH_SHORT).show()
 
             val intent = Intent(this, DetalheEmpresaActivity::class.java)
             startActivity(intent)
@@ -25,14 +30,19 @@ class MainActivity : AppCompatActivity() {
 
         button_servico.setOnClickListener {
 
+            Toast.makeText(this, "Serviços foi clicado!", Toast.LENGTH_SHORT).show()
+
             val intent = Intent(this, DetalheServicosActivity::class.java)
             startActivity(intent)
         }
 
         button_cliente.setOnClickListener {
 
+            Toast.makeText(this, "Clientes foi clicado!", Toast.LENGTH_SHORT).show()
+
             val intent = Intent(this, DetalheClientesActivity::class.java)
             startActivity(intent)
         }
+
     }
 }
